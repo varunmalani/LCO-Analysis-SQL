78a3fb38561a8b2a6614fc3038222387f140498a
Varun Malani <51241123+varunmalani@users.noreply.github.com>
2022-07-01 15:00:23 +0530

    Update lco_motors.sql

--- lco_motors/lco_motors.sql
+++ lco_motors/lco_motors.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 26, 2020 at 09:35 PM
--- Server version: 10.1.40-MariaDB
--- PHP Version: 7.3.5
-
 
 DROP DATABASE IF EXISTS `lco_motors`;
 
@@ -20,12 +11,6 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `lco_motors`
 --
 
