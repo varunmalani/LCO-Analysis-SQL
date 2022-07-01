f2eb2f78cc95dd5390f7498997c2f7a300bf1e23
Varun Malani <51241123+varunmalani@users.noreply.github.com>
2022-07-01 14:59:41 +0530

    Update lco_films.sql

--- lco_films/lco_films.sql
+++ lco_films/lco_films.sql
@@ -1,22 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 30, 2020 at 04:10 PM
--- Server version: 10.1.40-MariaDB
--- PHP Version: 7.3.5
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `lco_films`
